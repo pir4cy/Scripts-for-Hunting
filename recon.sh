f9f508a6a3c21c4be6efcf1791b6e2013e116812
Kshitij Tripathi <pir4cy@Kshitijs-iMac.local>
2020-07-16 15:06:27 +0530

    sudo for recon

--- recon.sh
+++ recon.sh
@@ -29,14 +29,14 @@ while read line; do
         banner "Recon Tool"
         
         #AMASS
-        amass enum -src -ip -active -d $line -o amassoutput.txt
+        sudo amass enum -src -ip -active -d $line -o amassoutput.txt
         cat amassoutput.txt | cut -d']' -f 2 | awk '{print $1}' | sort -u > hosts-amass.txt 
 
         #Subfinder
-        subfinder -d $line -o hosts-subfinder.txt -silent 
+        sudo subfinder -d $line -o hosts-subfinder.txt -silent 
 
         #AssetFinder
-        assetfinder --subs-only $line >> hosts-assetfinder.txt 
+        sudo assetfinder --subs-only $line >> hosts-assetfinder.txt 
 
         #Crt.sh
         curl -s "https://crt.sh/?q=%.$line&output=json" | jq '.[].name_value' | sed 's/\"//g' | sed 's/\*\.//g' | sort -u > hosts-crtsh.txt 
@@ -56,16 +56,16 @@ while read line; do
         #Checking for alive hosts
 
         #MassDNS
-        massdns -r $toolsFolder/massdns/lists/resolvers.txt -t A -o S -w $line-massdns.out hosts-scope.txt
+        sudo massdns -r $toolsFolder/massdns/lists/resolvers.txt -t A -o S -w $line-massdns.out hosts-scope.txt
         cat $line-massdns.out | awk '{print $1}' | sed 's/.$//' | sort -u > hosts-online.txt
 
         #httpx
-        httpx -l hosts-online.txt -title -content-length -status-code | tee httpx-out.txt
+        sudo httpx -l hosts-online.txt -title -content-length -status-code | tee httpx-out.txt
 
         #Checking for subdomain takeover
         clear
         banner "Subdomain Takeover"
-        subjack -w hosts-online.txt -t 1000 -o $line-takeover.txt -v
+        sudo subjack -w hosts-online.txt -t 1000 -o $line-takeover.txt -v
 
         #Masscan
         clear
@@ -73,7 +73,7 @@ while read line; do
         cat $line-massdns.out | awk '{print $3}' | sort -u | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" > ips-massdns.txt
         cat amassoutput.txt | cut -d']' -f 2 | awk '{print $2}' | sort -u > ips-amass.txt 
         cat ips-massdns.txt ips-amass.txt | sort -u > ips-online.txt
-        masscan -iL ips-online.txt --rate 10000 -p1-65535 --open-only --output-filename $line-masscan.out
+        sudo masscan -iL ips-online.txt --rate 10000 -p1-65535 --open-only --output-filename $line-masscan.out
 
         cd ..
         mv $line $line-done
